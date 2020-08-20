4a6129538f8832539a61ee365a18393ea84bd507
yuya yamamoto <yuya.yamamoto2000@gmail.com>
2020-08-20 16:58:29 +0900

    新規投稿画面作成

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,4 +2,23 @@ class BlogsController < ApplicationController
   def index
     @blog = Blog.all
   end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+    if params[:back]
+        render :new
+    else
+      if @blog.save
+        redirect_to new_blog_path, notice: "ブログを作成しました！"
+      else
+        render :new
+      end
+    end
+  end
+
+
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,3 @@
 class Blog < ApplicationRecord
+  validates :content, length: { in: 1..140, message: "1~140字の間で入力してください。"}
 end
